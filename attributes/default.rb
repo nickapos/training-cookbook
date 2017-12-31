7a4c435ef7b19bea867cb7cfea70a7b7792a641a
Nick Apostolakis <nickapos@oncrete.gr>
2017-12-31 09:22:53 +0000

    adding default attribute and recipe for packages installation and user creation

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['training_cookbook']['users'] = ['user1','user2','user3']
+default['training_cookbook']['packages'] = ['zsh','git','vim']
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+node['training_cookbook']['packages'].each do |packagename|
+  log "installing #{packagename}"
+  package packagename do
+    action :install
+  end
+end
+
+node['training_cookbook']['users'].each do |username|
+  log "creating user #{username}"
+  user username do
+    shell '/usr/bin/zsh'
+  end
+end
